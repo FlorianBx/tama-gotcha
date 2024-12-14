7ea4a55ad3723420e223a07de143fccc4412b992
Florian <25250981+BFlorian91@users.noreply.github.com>
2024-12-14 20:22:18 +0100

    refacto ♻️: display.ts

--- ui/display.ts
+++ ui/display.ts
@@ -1,13 +1,17 @@
-export function showMessage(message) {
-  document.getElementById('messages').textContent = message;
+import { Pet } from '../types/Pet'
+
+export function showMessage(message: string) {
+  const messagesElement = document.getElementById('messages')
+  if (messagesElement === null) return
+  messagesElement.textContent = message
 }
 
-export function checkAndShowWarnings(pet) {
+export function checkAndShowWarnings(pet: Pet) {
   if (pet.hunger < 20) {
-    showMessage('Ton animal a très faim ! 😢');
+    showMessage('Ton animal a très faim ! 😢')
   } else if (pet.happiness < 20) {
-    showMessage("Ton animal s'ennuie ! 😕");
+    showMessage("Ton animal s'ennuie ! 😕")
   } else if (pet.energy < 20) {
-    showMessage('Ton animal est fatigué ! 😴');
+    showMessage('Ton animal est fatigué ! 😴')
   }
 }
